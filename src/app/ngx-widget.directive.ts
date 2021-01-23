0847d1c9f57ebf06271a8db9bce665fa8ea76c70
Andrew Vitale <avitale@vhb.com>
2021-01-23 13:03:54 -0500

    (dep): migrate to es-lint

--- src/app/ngx-widget.directive.ts
+++ src/app/ngx-widget.directive.ts
@@ -1,8 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Directive({
-  selector: '[ngxWidget]',
+  selector: '[appNgxWidget]',
 })
-export class NgxWidgetDirective {
+export class NgxWidgetDirective implements OnInit, OnDestroy {
+  private readonly _destroy$ = new Subject<null>();
+
   constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this._destroy$.next(null);
+    this._destroy$.complete();
+  }
 }
